53f414d7869d70cd1738c1b737f02aba198558ff
anyars-encarta <anyarsencarta@gmail.com>
2024-05-03 12:24:12 +0000

    Create routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
-// import Login from "./pages/login/Login";
+import List from './pages/list/List';
+import Login from "./pages/login/Login";
+import New from "./pages/new/New";
+import Single from "./pages/single/Single";
 
 const App = () => {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/new' element={<New />} />
+          <Route path='/list' element={<List />} />
+          <Route path='/single' element={<Single />} />
+        </Routes>
+      </div>
+    </Router>
+
   );
 }
 
