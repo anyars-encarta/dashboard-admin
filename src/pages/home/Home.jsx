e2da341fa26f1df04848b1255c4bcf8b626127f2
anyars-encarta <anyarsencarta@gmail.com>
2024-05-03 16:18:27 +0000

    Install Material UI

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+// import Navbar from '../../components/navbar/Navbar';
+import Sidebar from '../../components/sidebar/Sidebar';
 import './home.scss';
 
 const Home = () => {
   return (
     <div className='home'>
-        Home
+      <Sidebar />
+      {/* <Navbar /> */}
+      <div className="homeContainer">
+        Home Container
+      </div>
     </div>
   )
 }
