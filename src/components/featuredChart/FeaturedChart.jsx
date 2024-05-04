15de8acbc131b98103616147bf70d0e4f88bffb7
anyars-encarta <anyarsencarta@gmail.com>
2024-05-04 17:34:12 +0000

    Implement left progress in home page

--- src/components/featuredChart/FeaturedChart.jsx
+++ src/components/featuredChart/FeaturedChart.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { KeyboardArrowDown, KeyboardArrowUpOutlined } from '@mui/icons-material';
 import './featuredChart.scss';
 
 const FeaturedChart = () => {
@@ -12,7 +15,37 @@ const FeaturedChart = () => {
 
       <div className="bottom">
         <div className="featuredDetail">
-          
+          <CircularProgressbar value={70} text={'70%'} strokeWidth={5}/>
+        </div>
+
+        <p className="title">Total Sales made today</p>
+        <p className="amount">$420</p>
+        <p className="desc">Previous transactions processing. Last payments may not be included.</p>
+
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult negative">
+              <KeyboardArrowDown fontSize='small'/>
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+
+          <div className="item">
+            <div className="itemTitle">Last Week</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlined fontSize='small'/>
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+
+          <div className="item">
+            <div className="itemTitle">Last Month</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlined fontSize='small'/>
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
