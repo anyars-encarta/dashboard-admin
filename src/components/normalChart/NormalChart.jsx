bbb4f2ba1ddba6898ebbee84912c44130ff2ebc6
anyars-encarta <anyarsencarta@gmail.com>
2024-05-04 18:16:48 +0000

    Add simple area chart

--- src/components/normalChart/NormalChart.jsx
+++ src/components/normalChart/NormalChart.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { data } from '../../constants/chartData';
 import './normalChart.scss';
 
 const NormalChart = () => {
   return (
-    <div className='normalChart'>NormalChart</div>
+    <div className='normalChart'><ResponsiveContainer width="100%" height="100%">
+      <AreaChart
+        width={500}
+        height={400}
+        data={data}
+        margin={{
+          top: 10,
+          right: 30,
+          left: 0,
+          bottom: 0,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+      </AreaChart>
+    </ResponsiveContainer></div>
   )
 }
 
