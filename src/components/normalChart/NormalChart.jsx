5d6df24b3766f7f51770ba3c66f67ed9a18d1eab
anyars-encarta <anyarsencarta@gmail.com>
2024-05-05 09:34:12 +0000

    Add income graph

--- src/components/normalChart/NormalChart.jsx
+++ src/components/normalChart/NormalChart.jsx
@@ -6,25 +6,25 @@ import './normalChart.scss';
 const NormalChart = () => {
   return (
     <div className='normalChart'>
-      <ResponsiveContainer width="100%" height="100%">
+      <div className="title">Last 6 Months (Income)</div>
+      <ResponsiveContainer width="100%" aspect={2 / 1}>
         <AreaChart width={730} height={250} data={data}
           margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
           <defs>
-            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
               <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
               <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
             </linearGradient>
-            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+            <linearGradient id="target" x1="0" y1="0" x2="0" y2="1">
               <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
               <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
             </linearGradient>
           </defs>
-          <XAxis dataKey="name" />
-          <YAxis />
-          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" stroke='gray' />
+          <CartesianGrid strokeDasharray="3 3" className='chartGrid' />
           <Tooltip />
-          <Area type="monotone" dataKey="uv" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
-          <Area type="monotone" dataKey="pv" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+          <Area type="monotone" dataKey="Total" stroke="#8884d8" fillOpacity={1} fill="url(#total)" />
+          <Area type="monotone" dataKey="Target" stroke="#82ca9d" fillOpacity={1} fill="url(#target)" />
         </AreaChart>
       </ResponsiveContainer></div>
   )
--- src/constants/chartData.js
+++ src/constants/chartData.js
@@ -1,44 +1,8 @@
 export const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
+    { name: 'Jan', Total: 1200, Target: 800 },
+    { name: 'Feb', Total: 2100, Target: 1600 },
+    { name: 'Mar', Total: 800, Target: 400 },
+    { name: 'Apr', Total: 1600, Target: 1000 },
+    { name: 'May', Total: 900, Target: 400},
+    { name: 'Jun', Total: 1700, Target: 1000 },
   ];
