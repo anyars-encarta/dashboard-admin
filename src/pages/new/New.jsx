df002471ea628e45262d4eaeb89071dbbe844f70
anyars-encarta <anyarsencarta@gmail.com>
2024-05-06 10:02:49 +0000

    Add item image url

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
 import { DriveFolderUploadOutlined } from '@mui/icons-material';
@@ -6,6 +6,8 @@ import { DriveFolderUploadOutlined } from '@mui/icons-material';
 import './new.scss';
 
 const New = ({ inputs, title }) => {
+  const [file, setFile] = useState('');
+
   return (
     <div className='new'>
       <Sidebar />
@@ -19,13 +21,13 @@ const New = ({ inputs, title }) => {
 
         <div className="bottom">
           <div className="left">
-            <img src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg" alt="" />
+            <img src={file ? URL.createObjectURL(file) : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"} alt="" />
           </div>
           <div className="right">
             <form>
               <div className="formInput">
                 <label for="file">Image: <DriveFolderUploadOutlined className='icon' /></label>
-                <input id='file' type="file" style={{ display: 'none' }} />
+                <input id='file' type="file" onChange={(e) => setFile(e.target.files[0])} style={{ display: 'none' }} />
               </div>
 
               {inputs.map(input => (
@@ -34,7 +36,6 @@ const New = ({ inputs, title }) => {
                   <input id='username' type={input.type} placeholder={input.placeholder} />
                 </div>
               ))}
-
 
               <button>Send</button>
             </form>
