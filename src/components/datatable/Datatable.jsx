e9ff9a7902adff4a0b5c67b2f41a38c259887f14
anyars-encarta <anyarsencarta@gmail.com>
2024-05-06 11:01:24 +0000

    Add delete user functionality

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import { userRows, userColumns } from '../../constants/tableData';
 import './datatable.scss';
 import { Link } from 'react-router-dom';
 
 const Datatable = () => {
+    const [data, setData] = useState(userRows);
+
+    const handleDelete = (id) => {
+        setData(data.filter((item) => item.id !== id));
+    };
 
     const actionColumn = [{
-        field: 'action', headerName: 'Action', width: 200, renderCell: () => {
+        field: 'action', headerName: 'Action', width: 200, renderCell: (params) => {
             return (
                 <div className='cellAction'>
-                    <Link to="/users/:userId" className='link'>
+                    <Link to={"/users/" + params.row.id} className='link'>
                         <span className="viewButton">View</span>
                     </Link>
                     <div>
-                        <span className="deleteButton">Delete</span>
+                        <span className="deleteButton" onClick={() => handleDelete(params.row.id)}>Delete</span>
                     </div>
                 </div>
             )
@@ -31,12 +36,12 @@ const Datatable = () => {
             </div>
             {/* <div style={{ height: 400, width: '100%' }}> */}
             <DataGrid
-                rows={userRows}
+                rows={data}
                 columns={userColumns.concat(actionColumn)}
                 disableRowSelectionOnClick
                 initialState={{
                     pagination: {
-                        paginationModel: { page: 0, pageSize: 9 },
+                        paginationModel: { page: 0, pageSize: 8 },
                     },
                 }}
                 pageSizeOptions={[5, 10]}
